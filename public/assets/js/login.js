9f71db1bb7463c5d209a0a61fbd8320775cf22c9
William Nicholson <will98nicholson@gmail.com>
2021-05-05 08:13:45 -0400

    login logout scripts

---
+++ public/assets/js/login.js
@@ -0,0 +1,51 @@
+const signupFunction = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/user-routes', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+const loginFunction = async (event) => {
+    event.preventDefault();
+
+    // Login values
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        // POST API request 
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // Successful login sends to profile handlebar
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFunction);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFunction);
