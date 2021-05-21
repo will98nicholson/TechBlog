2d8b4f8584d2d1bc87fa39cb2b54424b306db4cc
William Nicholson <will98nicholson@gmail.com>
2021-05-21 17:18:18 -0500

    jawsdb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('.utils/auth')
 const routes = require('./routes');
 
 const sequelize = require('./config/connection');
